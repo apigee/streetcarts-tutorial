5871952fbdddc4783433b47d4e7a8cdb4ee26c11
Steve Traut <straut@apigee.com>
2015-10-13 15:49:32 -0700

    Adding a function to add a new food item for a cart.
    Takes JSON in the shape of an item entity, including a cartID.

--- streetcarts/apiproxy/resources/node/data-manager.js
+++ streetcarts/apiproxy/resources/node/data-manager.js
@@ -8,197 +8,204 @@ var token = '';
 var path = '';
 
 module.exports = {
-	getAllCarts : function(args, callback){
-
-		endpointPath = '/foodcarts';
-		var uri = host + appPath + endpointPath;
-
-		var options = {
-			uri: uri,
-			method: "GET"
-		};
-
-		return makeRequest(options, function(error, response)
-		{
-			if (error) {
-				callback(error, null);
-			}
-			else {
-				callback(null, response);				
-			}
-		});
-	},
-	getCartsOwnedByUser : function(userUUID, callback){
-
-		endpointPath = '/users/' + userUUID + '/owns';		
-		var uri = host + appPath + endpointPath;
-
-		var options = {
-			uri: uri,
-			method: "GET"
-		};
-
-		return makeRequest(options, function(error, response)
-		{
-			if (error) {
-				callback(error, null);
-			}
-			else {
-				callback(null, response);				
-			}
-		});
-	},
-	getMenusForCart : function(cartUUID, callback){
-
-		endpointPath = '/foodcarts/' + cartUUID + '/publishes';
-		var uri = host + appPath + endpointPath;
-
-		var options = {
-			uri: uri,
-			method: "GET"
-		};
-
-		return makeRequest(options, function(error, response)
-		{
-			if (error) {
-				callback(error, null);
-			}
-			else {
-				callback(null, response);				
-			}
-		});
-	},
-	getItemsForMenu : function(menuUUID, callback){
-
-		endpointPath = '/menus/' + menuUUID + '/includes';
-		var uri = host + appPath + endpointPath;
-
-		var options = {
-			uri: uri,
-			method: "GET"
-		};
-
-		return makeRequest(options, function(error, response)
-		{
-			if (error) {
-				callback(error, null);
-			}
-			else {
-				callback(null, response);				
-			}
-		});
-	},
-	getDetailsForItem : function(itemUUID, callback){
-
-		endpointPath = "/items/" + itemUUID;
-		var uri = host + appPath + endpointPath;
-
-		var options = {
-			uri: uri,
-			method: "GET"
-		};
-
-		return makeRequest(options, function(error, response)
-		{
-			if (error) {
-				callback(error, null);
-			}
-			else {
-				callback(null, response);				
-			}
-		});
-	},
-	registerUser : function(userData, callback){
-
-		console.log(userData);
-
-		endpointPath = '/users';
-
-		// Make sure the client sent a username.
-		if (!userData.username){
-			console.log('No username set!');
-
-			var errorObject = new Error();
-			errorObject.message = "No username set. A unique username is required.";
-			errorObject.statusCode = "";
-			errorObject.errorType = "";
-
-			callback(errorObject, null);
-		} 
-		// Make sure the client sent a password.
-		if (userData.username && !userData.password){			
-			console.log('No password set!');
-
-			var errorObject = new Error();
-			errorObject.message = "No password set.";
-			errorObject.statusCode = "";
-			errorObject.errorType = "";
-
-			callback(errorObject, null);
-		}
-		// Try to create the user account.
-		else if (userData.username && userData.password) {
-			var uri = host + appPath + endpointPath;
-			var options = {
-				uri: uri,
-				body: JSON.stringify(userData),
-				method: "POST"
-			};
-			console.log("Options: " + JSON.stringify(options));
-			return makeRequest(options, function(error, response)
-			{
-				if (error) {
-					console.log("Got an error.");
-					callback(error, null);
-				}
-				else {
-					console.log("Success.");
-					callback(null, response);				
-				}
-			});
-		}
-	},
-	authenticateUser : function(credentials, callback){
-
-		endpointPath = "/token";
-		var uri = host + appPath + endpointPath;
-
-		var options = {
-			uri: uri,
-			body: JSON.stringify(credentials),
-			method: "POST"
-		};
-		return makeRequest(options, function(error, response)
-		{
-			if (error) {
-				callback(error, null);
-			}
-			else {
-				callback(null, response);				
-			}
-		});
-	}
+    getAllCarts: function (args, callback) {
+        
+        endpointPath = '/foodcarts';
+        var uri = host + appPath + endpointPath;
+        
+        var options = {
+            uri: uri,
+            method: "GET"
+        };
+        
+        return makeRequest(options, function (error, response) {
+            if (error) {
+                callback(error, null);
+            } else {
+                callback(null, response);
+            }
+        });
+    },
+    getCartsOwnedByUser: function (userUUID, callback) {
+        
+        endpointPath = '/users/' + userUUID + '/owns';
+        var uri = host + appPath + endpointPath;
+        
+        var options = {
+            uri: uri,
+            method: "GET"
+        };
+        
+        return makeRequest(options, function (error, response) {
+            if (error) {
+                callback(error, null);
+            } else {
+                callback(null, response);
+            }
+        });
+    },
+    getMenusForCart: function (cartUUID, callback) {
+        
+        endpointPath = '/foodcarts/' + cartUUID + '/publishes';
+        var uri = host + appPath + endpointPath;
+        
+        var options = {
+            uri: uri,
+            method: "GET"
+        };
+        
+        return makeRequest(options, function (error, response) {
+            if (error) {
+                callback(error, null);
+            } else {
+                callback(null, response);
+            }
+        });
+    },
+    getItemsForMenu: function (menuUUID, callback) {
+        
+        endpointPath = '/menus/' + menuUUID + '/includes';
+        var uri = host + appPath + endpointPath;
+        
+        var options = {
+            uri: uri,
+            method: "GET"
+        };
+        
+        return makeRequest(options, function (error, response) {
+            if (error) {
+                callback(error, null);
+            } else {
+                callback(null, response);
+            }
+        });
+    },
+    getDetailsForItem: function (itemUUID, callback) {
+        
+        endpointPath = "/items/" + itemUUID;
+        var uri = host + appPath + endpointPath;
+        
+        var options = {
+            uri: uri,
+            method: "GET"
+        };
+        
+        return makeRequest(options, function (error, response) {
+            if (error) {
+                callback(error, null);
+            } else {
+                callback(null, response);
+            }
+        });
+    },
+    addNewItem: function (itemData, callback) {
+        
+        var cartID = itemData.cartID;
+        
+        endpointPath = "/foodcarts/" + cartID + "/offers/items";
+        
+        var uri = host + appPath + endpointPath;
+        
+        var options = {
+            uri: uri,
+            body: JSON.stringify(itemData),
+            method: "POST"
+        };
+        
+        return makeRequest(options, function (error, response) {
+            if (error) {
+                callback(error, null);
+            } else {
+                callback(null, response);
+            }
+        });
+    },
+    registerUser: function (userData, callback) {
+        
+        console.log(userData);
+        
+        endpointPath = '/users';
+        
+        // Make sure the client sent a username.
+        if (! userData.username) {
+            console.log('No username set!');
+            
+            var errorObject = new Error();
+            errorObject.message = "No username set. A unique username is required.";
+            errorObject.statusCode = "";
+            errorObject.errorType = "";
+            
+            callback(errorObject, null);
+        }
+        // Make sure the client sent a password.
+        if (userData.username && ! userData.password) {
+            console.log('No password set!');
+            
+            var errorObject = new Error();
+            errorObject.message = "No password set.";
+            errorObject.statusCode = "";
+            errorObject.errorType = "";
+            
+            callback(errorObject, null);
+        }
+        // Try to create the user account. else if (userData.username && userData.password) {
+        var uri = host + appPath + endpointPath;
+        var options = {
+            uri: uri,
+            body: JSON.stringify(userData),
+            method: "POST"
+        };
+        return makeRequest(options, function (error, response) {
+            if (error) {
+                console.log("Got an error.");
+                callback(error, null);
+            } else {
+                console.log("Success.");
+                callback(null, response);
+            }
+        });
+    },
+    authenticateUser: function (credentials, callback) {
+        
+        endpointPath = "/token";
+        var uri = host + appPath + endpointPath;
+        
+        var options = {
+            uri: uri,
+            body: JSON.stringify(credentials),
+            method: "POST"
+        };
+        return makeRequest(options, function (error, response) {
+            if (error) {
+                callback(error, null);
+            } else {
+                callback(null, response);
+            }
+        });
+    }
 };
 
 
-function makeRequest(options, callback){
-
-	request(options, function(error, response, body){
-
-		if (response.statusCode != 200) {
-
-			var responseBody = JSON.parse(body);
-
-			var errorObject = new Error();
-			errorObject.message = responseBody.error_description;
-			errorObject.statusCode = response.statusCode;
-			errorObject.errorType = responseBody.error;
-
-			callback(errorObject, response);
-		}
-		else {
-			callback(null, body);
-		}
-	});
+function makeRequest(options, callback) {
+    
+    request(options, function (error, response, body) {
+        
+        var errorObject = new Error();
+        
+        if (error) {
+            errorObject.message = error.message;
+            callback(errorObject, null);
+        } else if (response && response.statusCode != 200) {
+            var responseBody = JSON.parse(body);
+            
+            var errorObject = new Error();
+            errorObject.message = responseBody.error_description;
+            errorObject.statusCode = response.statusCode;
+            errorObject.errorType = responseBody.error;
+            
+            callback(errorObject, response);
+        } else {
+            callback(null, body);
+        }
+    });
 }
-
--- streetcarts/apiproxy/resources/node/server.js
+++ streetcarts/apiproxy/resources/node/server.js
@@ -1,109 +1,128 @@
-var dataManager = require( './data-manager' );
+var dataManager = require('./data-manager');
 var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json());
+// support json encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// support encoded bodies
 
 // Free public APIs
 
 app.get('/carts', function (req, res) {
-  console.log('/carts');
-  dataManager.getAllCarts('', function(error, data) {
-    var expand = req.query.expand;
-    
-    if (error){
-      res.send(error);
-    }
-    if (data){
-      if (expand !== undefined && expand == 'true') {
-        res.set('Content-Type', 'application/json');
-        res.send(data); 
-      } else {
-
-        var jsonData = JSON.parse(data);
-        var cartdata = {
-          cartinfo: []
-        };
-
-        for (var i = 0; i < jsonData.entities.length; i++) {
-          cartdata.cartinfo.push({
-             "name": jsonData.entities[i].name,
-             "uuid": jsonData.entities[i].uuid,
-             "city": jsonData.entities[i].location.city
-          });
+    console.log('/carts');
+    dataManager.getAllCarts('', function (error, data) {
+        var expand = req.query.expand;
+        
+        if (error) {
+            res.send(error);
         }
-      res.set('Content-Type', 'application/json');
-      res.send(JSON.stringify(cartdata));
-      }
-    }
-  });
+        if (data) {
+            if (expand !== undefined && expand == 'true') {
+                res.set('Content-Type', 'application/json');
+                res.send(data);
+            } else {
+                
+                var jsonData = JSON.parse(data);
+                var cartdata = {
+                    cartinfo:[]
+                };
+                
+                for (var i = 0; i < jsonData.entities.length; i++) {
+                    cartdata.cartinfo.push({
+                        "name": jsonData.entities[i].name,
+                        "uuid": jsonData.entities[i].uuid,
+                        "city": jsonData.entities[i].location.city
+                    });
+                }
+                res.set('Content-Type', 'application/json');
+                res.send(JSON.stringify(cartdata));
+            }
+        }
+    });
 });
 
 
 app.get('/carts/:uuid/menus', function (req, res) {
-  var uuid = req.params.uuid;
-  console.log('/carts/' + uuid + '/menus');
-  dataManager.getMenusForCart(uuid, function(error, data) {
-    if (error){
-      res.send(error);
-    }
-    if (data){
-      res.set('Content-Type', 'application/json');
-      res.send(data);      
-    }
-  });
+    var uuid = req.params.uuid;
+    console.log('/carts/' + uuid + '/menus');
+    dataManager.getMenusForCart(uuid, function (error, data) {
+        if (error) {
+            res.send(error);
+        }
+        if (data) {
+            res.set('Content-Type', 'application/json');
+            res.send(data);
+        }
+    });
 });
 
 
 //*** APIs for registered cart owners.
 
 app.get('/users/:uuid/carts', function (req, res) {
-  //var uuid = "4ab08a6a-6d16-11e5-817d-a9b5da1cd192";
-  var uuid = req.params.uuid;
-  console.log('/users/' + uuid + '/carts');
-  dataManager.getCartsOwnedByUser(uuid, function(error, data) {
-    if (error){
-      res.send(error);
-    }
-    if (data){
-      res.set('Content-Type', 'application/json');
-      res.send(data);
-    }
-  });
+    //var uuid = "4ab08a6a-6d16-11e5-817d-a9b5da1cd192";
+    var uuid = req.params.uuid;
+    console.log('/users/' + uuid + '/carts');
+    dataManager.getCartsOwnedByUser(uuid, function (error, data) {
+        if (error) {
+            res.send(error);
+        }
+        if (data) {
+            res.set('Content-Type', 'application/json');
+            res.send(data);
+        }
+    });
+});
+
+app.post('/carts/:uuid/items', function (req, res) {
+    var uuid = req.params.uuid;
+    var itemData = req.body;
+    dataManager.addNewItem(itemData, function (error, data) {
+        if (error) {
+            res.send(error);
+        }
+        if (data) {
+            res.set('Content-Type', 'application/json');
+            res.send(data);
+        }
+    });
 });
 
 
 app.post('/users', function (req, res) {
-
-  var username = req.body.username;
-  var password = req.body.password;
-  var email, city;
-  if (req.body.email) {
-     email = req.body.email;
-  } else {
-    email = "Not Given";
-  }
-  if (req.body.city) {
-     city = req.body.city;
-  } else {
-    city = "Not Given";
-  }
-  
-  var userdata = {"username" : username, "password": password, "email": email, "city": city};
-
-
-  dataManager.registerUser(userdata, function(error, data) {
-    if (error){
-      res.send(error);
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    var email, city;
+    if (req.body.email) {
+        email = req.body.email;
+    } else {
+        email = "Not Given";
     }
-    if (data){
-      res.set('Content-Type', 'application/json');
-      res.send(data);
+    if (req.body.city) {
+        city = req.body.city;
+    } else {
+        city = "Not Given";
     }
-  });
-
+    
+    var userdata = {
+        "username": username, "password": password, "email": email, "city": city
+    };
+    
+    
+    dataManager.registerUser(userdata, function (error, data) {
+        if (error) {
+            res.send(error);
+        }
+        if (data) {
+            res.set('Content-Type', 'application/json');
+            res.send(data);
+        }
+    });
 });
 
 
@@ -112,9 +131,8 @@ app.post('/users', function (req, res) {
 
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log('Example app listening at http://%s:%s', host, port);
 });
- 
