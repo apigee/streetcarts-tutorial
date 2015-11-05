6278d0864f6c7d34df1d40f309545847fdc4cd6d
Floyd Jones <fjones@apigee.com>
2015-11-05 12:31:34 -0700

    enabling 'clean'

--- streetcarts/proxies/src/gateway/streetcarts_build.sh
+++ streetcarts/proxies/src/gateway/streetcarts_build.sh
@@ -17,31 +17,31 @@ cd apps/streetcarts/proxies/src/gateway
 # The -Doptions=clean undeploys and deletes the existing revision
 
 cd accesstoken
-# mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
+mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
 mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org
 rm -r target
 cd ..
 
 cd foodcarts
-# mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
+mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
 mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org
 rm -r target
 cd ..
 
 cd items
-# mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
+mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
 mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org
 rm -r target
 cd ..
 
 cd menus
-# mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
+mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
 mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org
 rm -r target
 cd ..
 
 cd reviews
-# mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
+mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
 mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org
 rm -r target
 cd ..
@@ -53,7 +53,7 @@ rm -r target
 cd ..
 
 cd users
-# mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
+mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org -Doptions=clean
 mvn install -P test -Dusername=$username -Dpassword=$password -Dorg=$org
 rm -r target
 cd ..
