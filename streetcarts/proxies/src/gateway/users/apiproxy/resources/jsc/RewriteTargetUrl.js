e82dec498ab11044c1c4ee7d07974940dd871ef0
wwitman <wwitman@apigee.com>
2015-11-05 15:40:41 -0700

    add path suffix

--- streetcarts/proxies/src/gateway/users/apiproxy/resources/jsc/RewriteTargetUrl.js
+++ streetcarts/proxies/src/gateway/users/apiproxy/resources/jsc/RewriteTargetUrl.js
@@ -1,3 +1,11 @@
  var target = context.getVariable("target.url");
+ var suffix = context.getVariable("proxy.pathsuffix");
+ print(suffix);
+ 
  var newTarget = target+"/users";
  context.setVariable("target.url", newTarget);
+ 
+ if (suffix !== "") {
+     newTarget = newTarget + suffix;
+     context.setVariable("target.url", newTarget);
+ }
