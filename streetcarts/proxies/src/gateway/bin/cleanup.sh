4a2203ec54b78baa7cb7d1f1e8a978862bf5e098
wwitman <wwitman@apigee.com>
2015-11-11 08:00:42 -0700

    add data manager entities

--- streetcarts/proxies/src/gateway/bin/cleanup.sh
+++ streetcarts/proxies/src/gateway/bin/cleanup.sh
@@ -2,9 +2,9 @@
 
 echo -e "\n**** Deleting Apps"
 
-curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/SC-OWNER-APP -X DELETE
-
-curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/SC-PUBLIC-APP -X DELETE
+curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/sc-owner-app -X DELETE
+curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/sc-public-app -X DELETE
+curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/SC-DATA-MANAGER-AUTH-APP -X DELETE
 
 
 echo -e "\n**** Deleting Developers"
@@ -14,7 +14,8 @@ curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com -X DE
 
 echo -e "\n**** Deleting Products"
 
-curl -u $username:$password $url/v1/o/$org/apiproducts/SC-PUBLIC-PRODUCT -X DELETE
-curl -u $username:$password $url/v1/o/$org/apiproducts/SC-OWNER-PRODUCT -X DELETE
+curl -u $username:$password $url/v1/o/$org/apiproducts/sc-public-product -X DELETE
+curl -u $username:$password $url/v1/o/$org/apiproducts/sc-owner-product -X DELETE
+curl -u $username:$password $url/v1/o/$org/apiproducts/SC-DATA-MANAGER-AUTH-PRODUCT -X DELETE
 
 echo -e "\n**** Cleanup Completed"
