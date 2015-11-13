4701a71494ab15cf3548d8a3f8a2f6991852f539
wwitman <wwitman@apigee.com>
2015-11-13 10:15:13 -0700

    added dm entities

--- streetcarts/proxies/src/gateway/bin/setup.sh
+++ streetcarts/proxies/src/gateway/bin/setup.sh
@@ -18,6 +18,11 @@ curl -u $username:$password $url/v1/o/$org/apiproducts \
   -H "Content-Type: application/json" -X POST -T ./entities/SC-PUBLIC-PRODUCT.json
 
 
+  echo  -e "\n**** CREATING SC-DATA-MANAGER-PRODUCT"
+curl -u $username:$password $url/v1/o/$org/apiproducts \
+  -H "Content-Type: application/json" -X POST -T ./entities/SC-DATA-MANAGER-PRODUCT.json
+
+
 # Create Developer Apps
 
 echo  -e "\n**** CREATING SC-OWNER-APP"
@@ -30,6 +35,11 @@ echo  -e "\n**** CREATING SC-PUBLIC-APP"
 curl -u $username:$password \
   $url/v1/o/$org/developers/will@streetcarts.com/apps \
   -H "Content-Type: application/json" -X POST -T ./entities/SC-PUBLIC-APP.json
+
+echo  -e "\n**** CREATING SC-DATA-MANAGER-APP"
+curl -u $username:$password \
+  $url/v1/o/$org/developers/will@streetcarts.com/apps \
+  -H "Content-Type: application/json" -X POST -T ./entities/SC-DATA-MANAGER-APP.json
 
 
 
@@ -75,6 +85,30 @@ curl -u $username:$password \
 echo -e "\n**** BASE64 ENCODE THE KEY:SECRET FOR THE PUBLIC APP"
 auth=$(echo -ne "$key:$secret" | base64);
 echo -e "\n**** Base64 encoded credentials:  $auth ****"
+
+
+
+echo -e "\n**** GET KEYS FROM THE DATA-MANAGER APP"
+
+ks=`curl -u "$username:$password" "$url/v1/o/$org/developers/will@streetcarts.com/apps/SC-DATA-MANAGER-APP" 2>/dev/null | egrep "consumer(Key|Secret)"`
+key=`echo $ks | awk -F '\"' '{ print $4 }'`
+secret=`echo $ks | awk -F '\"' '{ print $8 }'`
+
+
+echo -e "\n**** Got Consumer Key for Data Manager App: $key ****"
+echo -e "\n**** Got Consumer Secret for Data Manager App: $secret ****"
+
+echo -e "\n**** ASSOCIATE THE KEY WITH THE DATA MANAGER PRODUCT"
+curl -u $username:$password \
+  $url/v1/o/$org/developers/will@streetcarts.com/apps/SC-DATA-MANAGER-APP/keys/${key} \
+  -H "Content-Type: application/json" -X POST -d '{"apiproducts": ["{SC-DATA-MANAGER-PRODUCT}"]}'
+
+echo -e "\n**** BASE64 ENCODE THE KEY:SECRET FOR THE DATA-MANAGER APP"
+auth=$(echo -ne "$key:$secret" | base64);
+echo -e "\n**** Base64 encoded credentials:  $auth ****"
+
+
+
 
 echo "DONE CREATING ENTITIES"
 
