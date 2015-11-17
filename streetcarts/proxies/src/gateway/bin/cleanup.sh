4b314fb64263232be6a7323e04bf752c2ec9df08
wwitman <wwitman@apigee.com>
2015-11-16 18:09:31 -0700

    fixed dm entity names

--- streetcarts/proxies/src/gateway/bin/cleanup.sh
+++ streetcarts/proxies/src/gateway/bin/cleanup.sh
@@ -4,7 +4,7 @@ echo -e "\n**** Deleting Apps"
 
 curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/sc-owner-app -X DELETE
 curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/sc-public-app -X DELETE
-curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/SC-DATA-MANAGER-AUTH-APP -X DELETE
+curl -u $username:$password $url/v1/o/$org/developers/will@streetcarts.com/apps/SC-DATA-MANAGER-APP -X DELETE
 
 
 echo -e "\n**** Deleting Developers"
@@ -16,6 +16,6 @@ echo -e "\n**** Deleting Products"
 
 curl -u $username:$password $url/v1/o/$org/apiproducts/sc-public-product -X DELETE
 curl -u $username:$password $url/v1/o/$org/apiproducts/sc-owner-product -X DELETE
-curl -u $username:$password $url/v1/o/$org/apiproducts/SC-DATA-MANAGER-AUTH-PRODUCT -X DELETE
+curl -u $username:$password $url/v1/o/$org/apiproducts/SC-DATA-MANAGER-PRODUCT -X DELETE
 
 echo -e "\n**** Cleanup Completed"
