507d52478da7a7f48a6d6ee6f68bf9e373900384
wwitman <wwitman@apigee.com>
2015-11-17 14:55:02 -0700

    latest updates

--- streetcarts/proxies/src/gateway/users/apiproxy/resources/jsc/RewriteTargetUrl.js
+++ streetcarts/proxies/src/gateway/users/apiproxy/resources/jsc/RewriteTargetUrl.js
@@ -1,9 +1,7 @@
  var target = context.getVariable("target.url");
  var suffix = context.getVariable("proxy.pathsuffix");
-
- print("target.url: " + target)
- print("proxy.pathsuffix: " + suffix);
+ var dmKey = context.getVariable("DATA-MANAGER-KEY");
  
- var targetRewrite = target + "/users" + suffix  + "?apikey=JBjbg7SL1dHfRJPE3AuFUewGS6k9LTgD";
- print("target.rewrite: " + targetRewrite);
+ context.setVariable("request.header.x-api-key", dmKey);
+ var targetRewrite = target + "/users" + suffix;
  context.setVariable("target.url", targetRewrite);
