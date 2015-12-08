ca2752d11064844b5b26f7f325946e7bc66c11a8
wwitman <wwitman@apigee.com>
2015-12-08 11:44:46 -0700

    passing user uuid to data manager in a header

--- streetcarts/proxies/src/gateway/foodcarts/apiproxy/resources/jsc/RewriteTargetUrl.js
+++ streetcarts/proxies/src/gateway/foodcarts/apiproxy/resources/jsc/RewriteTargetUrl.js
@@ -1,8 +1,10 @@
  var target = context.getVariable("target.url");
  var suffix = context.getVariable("proxy.pathsuffix");
  var dmKey = context.getVariable("DATA-MANAGER-KEY");
+ var userId = context.getVariable('accesstoken.USER_UUID');
  print("KEY: " + dmKey);
  
+ context.setVariable("request.header.x-user-uuid", userId);
  context.setVariable("request.header.x-api-key", dmKey);
  var targetRewrite = target + "/foodcarts" + suffix;
  context.setVariable("target.url", targetRewrite);
