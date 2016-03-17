94eec8ff35b471885b5f72238fe991e01a565ca0
Steve Traut <straut@apigee.com>
2016-03-17 13:17:37 -0400

    Adding some comments and handling of a null value (while I figure out how to raise a fault from within JS).

--- streetcarts/proxies/src/gateway/accesstoken/apiproxy/resources/jsc/SetScope.js
+++ streetcarts/proxies/src/gateway/accesstoken/apiproxy/resources/jsc/SetScope.js
@@ -5,20 +5,28 @@
 var userGroups = JSON.parse(context.getVariable("streetcarts.user.groups"));
 var userScope = "";
 
-if (userGroups.user_groups && userGroups.user_groups.length > 0) {
-    for (var i = 0; i < userGroups.user_groups.length; i++) {
-        var groupName = userGroups.user_groups[i].name;
-        if (groupName === "owners") {
-            userScope = userScope + "owner.read owner.create owner.delete owner.update ";
+print('userGroups: ' + userGroups);
+
+if (userGroups) {
+    /**
+     * Set scope values based on which data store user groups 
+     * the user is a member of.
+     */
+    if (userGroups.user_groups && userGroups.user_groups.length > 0) {
+        for (var i = 0; i < userGroups.user_groups.length; i++) {
+            var groupName = userGroups.user_groups[i].name;
+            if (groupName === "owners") {
+                userScope = userScope + "owner.read owner.create owner.delete owner.update ";
+            }
+            if (groupName.indexOf("/managers/menus") > 0) {
+                userScope = userScope + "manager.read manager.update ";
+            }
+            if (groupName === "members") {
+                userScope = userScope + "reviewer.read reviewer.create ";
+            }
         }
-        if (groupName.indexOf("/managers/menus") > 0) {
-            userScope = userScope + "manager.read manager.update ";
-        }
-        if (groupName === "members") {
-            userScope = userScope + "reviewer.read reviewer.create ";
-        }
+        // Trim the scope value and set it to a variable for retrieval later.
+        userScope = userScope.replace(/(^[,\s]+)|([,\s]+$)/g, '');
+        context.setVariable("streetcarts.user.scope", userScope);
     }
 }
-
-userScope = userScope.replace(/(^[,\s]+)|([,\s]+$)/g, '');
-context.setVariable("streetcarts.user.scope", userScope);
